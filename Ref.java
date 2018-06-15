de1236b8f7a797f2ae196a79dc2256fb85ea2a59
Adam Bell <adambb2@gmail.com>
2018-06-15 00:30:25 -0400

    Update Ref.java

--- Ref.java
+++ Ref.java
@@ -1,7 +1,7 @@
 package com.Adamsbot.Adambbbot;
 
 public class Ref {
-	public static final String token="NDU2MjcxNTY2NDU2NzUwMDkw.DgIHkg.cZIc4HGPPoQqxbaFp51sBhqZYm0";
+	public static final String token="my token"; //The token is unique to my bot so I am not disclosing it here
 	public static final String prefix = ">";
 
 }
