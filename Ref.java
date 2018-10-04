a1c7c7ff77be8c9e89c4b1f967d18add97abfad5
Adam Bell <adambb2@gmail.com>
2018-10-04 18:16:25 -0400

    Delete Ref.java
    old version

--- Ref.java
+++
@@ -1,7 +0,0 @@
-package com.Adamsbot.Adambbbot;
-
-public class Ref {
-	public static final String token="my token"; //The token is unique to my bot so I am not disclosing it here
-	public static final String prefix = ">";
-
-}
